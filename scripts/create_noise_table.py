13fd528b9f798e70d587a5efe011e2956f6a9c95
Mateusz Sokół <mat646@gmail.com>
2021-03-20 23:43:02 +0100

    Modified promotion logic

--- scripts/create_noise_table.py
+++ scripts/create_noise_table.py
@@ -1,21 +1,8 @@
 import numpy as np
-import multiprocessing as mp
-import ctypes
-
-
-def shared_noise_table() -> np.ndarray:
-    seed = 123
-    count = 250000000  # 1 gigabyte of 32-bit numbers. Will actually sample 2 gigabytes below.
-    print('Sampling {} random numbers with seed {}'.format(count, seed))
-    shared_mem = mp.Array(ctypes.c_float, count, lock=False)
-    noise = np.ctypeslib.as_array(shared_mem)
-    assert noise.dtype == np.float32
-    noise[:] = np.random.RandomState(seed).randn(count)  # 64-bit to 32-bit conversion here
-    print('Sampled {} bytes'.format(noise.size * 4))
-    return noise
 
 
 if __name__ == '__name__':
-
-    noise = shared_noise_table()
+    seed = 123
+    count = 250000000
+    noise = np.random.RandomState(seed).randn(count)
     np.save('noise.npy', noise)
