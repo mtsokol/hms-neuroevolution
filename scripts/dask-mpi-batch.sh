0b73f68ceebcb76311589ec256513f17b96a6e10
Mateusz Sokół <mat646@gmail.com>
2021-03-29 20:19:48 +0200

    Added assets

--- scripts/dask-mpi-batch.sh
+++ scripts/dask-mpi-batch.sh
@@ -4,10 +4,10 @@
 #SBATCH -p plgrid-short
 #SBATCH -A <GRANT_ID>
 #SBATCH --ntasks-per-node=24
-#SBATCH --mem=48G
+#SBATCH --mem=55G
 #SBATCH -t 01:00:00
-#SBATCH --nodes=24
+#SBATCH --nodes=10
 
 export NOISE_PATH=/net/archive/groups/plgghmsneuro/noise.npy
 module load plgrid/libs/python-mpi4py/3.0.1-python-3.6
-mpirun -np 576 python3 -m implementation.experiments.hms_atari_sea -e 50
+mpirun -np 240 python3 -m implementation.experiments.hms_atari_sea -e 60
